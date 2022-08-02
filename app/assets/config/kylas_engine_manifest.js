b16e427046a9b0d4a9724205c90cf98d55a39e4c
shyampandav <shyam.pandav@sell.do>
2022-08-03 00:32:38 +0530

    Add-files-in-precompile

--- app/assets/config/kylas_engine_manifest.js
+++ app/assets/config/kylas_engine_manifest.js
@@ -1,3 +1,5 @@
 //= link_tree ../images/kylas_engine
 //= link_directory ../stylesheets/kylas_engine .scss
 //= link_directory ../javascript/kylas_engine .js
+//= link kylas_engine/application.css
+//= link kylas_engine/application.js
